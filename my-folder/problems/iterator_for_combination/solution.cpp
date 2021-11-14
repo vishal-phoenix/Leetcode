807f7c1cf2929708cdfe84f6f78f95679a3f39ec
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2021-11-14 13:46:29 +0000

    Sync LeetCode submission - Iterator for Combination (cpp)

---
+++ my-folder/problems/iterator_for_combination/solution.cpp
@@ -0,0 +1,38 @@
+class CombinationIterator {
+    string s;
+    queue<string> q;
+    
+    void getCombination(int start, int length, string txt){
+        if(length == 0){
+            q.push(txt);
+            return;
+        }
+        
+        for(int i=start; i <=s.length() - length;++i){
+            getCombination(i+1, length-1, txt+s[i]);
+        }
+    }
+public:
+    CombinationIterator(string characters, int combinationLength) {
+        s = characters;
+        string txt = "";
+        getCombination(0, combinationLength, txt);
+    }
+    
+    string next() {
+        string str = q.front();
+        q.pop();
+        return str;
+    }
+    
+    bool hasNext() {
+        return !q.empty();
+    }
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */

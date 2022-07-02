f9ecc169029f474a1e8df541e5fd007a657d01b5
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-07-02 09:34:42 +0000

    Sync LeetCode submission - Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (cpp)

---
+++ my-folder/problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        int maxhor = 0, maxver = 0;
+        int sizhor = horizontalCuts.size();
+        int sizver = verticalCuts.size();
+        sort(horizontalCuts.begin(),horizontalCuts.end());
+        sort(verticalCuts.begin(),verticalCuts.end());
+        for(int i=0;i<sizhor;i++){
+            int def = 0;
+            if(i==0){
+                def = horizontalCuts[0];
+            }else{
+                def = horizontalCuts[i]-horizontalCuts[i-1];
+            }
+            if(def>=maxhor){
+                maxhor = def;
+            }
+        }
+        int def2 = h - horizontalCuts[sizhor-1];
+        if(def2>=maxhor){
+            maxhor = def2;
+        }
+        for(int i=0;i<sizver;i++){
+            int def = 0;
+            if(i==0){
+                def = verticalCuts[0];
+            }else{
+                def = verticalCuts[i]-verticalCuts[i-1];
+            }
+            if(def>=maxver){
+                maxver = def;
+            }
+        }
+        int def3 = w - verticalCuts[sizver-1];
+        if(def3>=maxver){
+            maxver = def3;
+        }
+        long long ans = 1;
+        ans = (ans*maxhor)%1000000007;
+        ans = (ans*maxver)%1000000007;
+        return ans;
+    }
+};

106ae5964a6798e29470cff1a05b9f97c24b8d0f
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-07-05 17:13:44 +0530

    Create Queue_reconstruction_height.cpp

---
+++ Queue_reconstruction_height.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Solution S;
+    vector<vector<int>> people{{7,0},{4,4},{7,1},{5,2},{6,1},{5,0}};
+    // cout<<S.reconstructQueue(people);
+    sort(people.begin(),people.end());
+
+    int sz = people.size();
+    vector<vector<int>> nwpepl(sz,vector<int>(2,-1));
+    for(int i=0;i<sz;i++){
+        for(int j=0;j<2;j++){
+            cout<<nwpepl[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<sz;i++){
+        int cnt = people[i][1];
+        for(int j=0;j<sz;j++){
+            if(nwpepl[j][0]==-1 && cnt == 0){
+                nwpepl[j][0] = people[i][0];
+                nwpepl[j][1] = people[i][1];
+                break;
+            }else if(nwpepl[j][0]==-1 || nwpepl[j][0]>=people[i][0]){
+                cnt--;
+            }
+        }
+    }
+
+    return 0;
+}

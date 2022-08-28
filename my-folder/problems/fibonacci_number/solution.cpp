1828cb239e6abcf754327d9b9c337020fcf0dba3
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-08-28 13:33:41 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- my-folder/problems/fibonacci_number/solution.cpp
+++ my-folder/problems/fibonacci_number/solution.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> fb(n+1);
-        
         if(n==0){
             return 0;
-        }else if(n==1){
-            return 1;
-        }else{
-            fb[0] = 0;
-            fb[1] = 1;
-            for(int i=2;i<=n;i++){
-                fb[i] = fb[i-1] + fb[i-2];
-            }
-            return fb[n];
         }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
     }
 };

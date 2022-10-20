9e90edf8cbcd2aad45b3ad057ca6e65b54cab953
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-10-20 08:43:45 +0000

    Sync LeetCode submission - Palindrome Linked List (cpp)

--- my-folder/problems/palindrome_linked_list/solution.cpp
+++ my-folder/problems/palindrome_linked_list/solution.cpp
@@ -11,26 +11,23 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> ans;
-        while(head!=nullptr){
-            ans.push_back(head->val);
-            head=head->next;
+        if(head == NULL){
+            return 0;
         }
-        int x = ans.size();
-        if(x%2==0){
-            for(int i=0;i<x/2;i++){
-                if(ans[i]!=ans[x-1-i]){
-                    return false;
-                }
-            }
+        ListNode* temp = head;
+        string t = "";
+        while(temp!=NULL){
+            int x = temp->val;
+            string c = to_string(x);
+            t+=c;
+            temp = temp->next;
+        }
+        string g = t;
+        reverse(g.begin(),g.end());
+        if(g==t){
+            return 1;
         }else{
-            int y=x/2;
-            for(int i=0;i<y;i++){
-                if(ans[i]!=ans[x-1-i]){
-                    return false;
-                }
-            }
+            return 0;
         }
-        return true;
     }
 };

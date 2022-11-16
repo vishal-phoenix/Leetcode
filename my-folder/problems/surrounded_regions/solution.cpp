3a63c84d8f3adf97f95ac85d5fb83fbf83f7d480
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-11-16 05:44:05 +0000

    Sync LeetCode submission - Surrounded Regions (cpp)

--- my-folder/problems/surrounded_regions/solution.cpp
+++ my-folder/problems/surrounded_regions/solution.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
+    void dfs(int i,int j,vector<vector<char>>& board, vector<vector<int>>& vs ){
+        vs[i][j]=1;
+        if(i+1>=0 && i+1<board.size() && j>=0 && j<board[0].size() && board[i+1][j]=='O'){
+            if(!vs[i+1][j]){
+                dfs(i+1,j,board,vs);
+            }
+        }
+        if(i-1>=0 && i-1<board.size() && j>=0 && j<board[0].size() && board[i-1][j]=='O'){
+            if(!vs[i-1][j]){
+                dfs(i-1,j,board,vs);
+            }
+        }
+        if(i>=0 && i<board.size() && j+1>=0 && j+1<board[0].size() && board[i][j+1]=='O'){
+            if(!vs[i][j+1]){
+                dfs(i,j+1,board,vs);
+            }
+        }
+        if(i>=0 && i<board.size() && j-1>=0 && j-1<board[0].size() && board[i][j-1]=='O'){
+            if(!vs[i][j-1]){
+                dfs(i,j-1,board,vs);
+            }
+        }
+        return;
+    }
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        if(m<=2) return;
-        int n=board[0].size();
-        if(n <= 2) return;
-        
-        for(int i=0;i<m;++i){
-            for (int j = 0;j<n;++j){
-                if(board[i][j] == 'O' && (i==0 || i== m-1 || j == 0 || j == n-1)){
-                    dfs(board, i, j);
+        int n = board.size(),m = board[0].size();
+        vector<vector<int>> vs(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(!vs[i][j] && board[i][j]=='O'){
+                        dfs(i,j,board,vs);
+                    }
                 }
             }
         }
-        for(int i=0;i<m;++i){
-            for (int j=0;j<n;++j){
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                else if(board[i][j] == 'A') board[i][j] = 'O';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && !vs[i][j]){
+                    board[i][j] = 'X';
+                }
             }
         }
-        
-    }
-    void dfs (vector<vector<char>> &board, int i, int j){
-        if(i >= 0 && i< board.size() && j>=0 && j<board[0].size() && board[i][j] == 'O'){
-            board[i][j] = 'A';
-            dfs(board, i+1, j);
-            dfs(board, i-1, j);
-            dfs(board, i, j+1);
-            dfs(board, i, j-1);
-            
-        }
+        return;
     }
 };

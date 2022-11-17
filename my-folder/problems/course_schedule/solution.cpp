91b9285a30f7d9eab8f58a82cdcc268c524a64d0
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2022-11-17 10:59:04 +0000

    Sync LeetCode submission - Course Schedule (cpp)

---
+++ my-folder/problems/course_schedule/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> a(numCourses,0);
+        vector<int> adj[numCourses];
+
+        
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        for(int i=0;i<numCourses;i++){
+            for(auto it: adj[i]){
+                a[it]++;
+            }
+        }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0;i<numCourses;i++){
+            if(a[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+            for(auto it: adj[x]){
+                a[it]--;
+                if(a[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()==numCourses){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};

93d9dddd281f0dd388daf08455da0ade54fa87b8
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2023-02-19 16:06:34 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans(2);
-        map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]] = i;
+    vector<int> twoSum(vector<int>& a, int t) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<a.size();i++){
+            mp[a[i]]++;
         }
-        for(int i=0;i<n;i++){
-            int n = nums[i];
-            int x = target-nums[i];
-            if(mp.count(x)){
-                if(mp[x] != i){
-                    ans[0] = i;
-                    ans[1] = mp[x];
+        int b=-1,c=-1;
+        for(auto it:mp){
+            int x = it.first;
+            int y = it.second;
+            int z = t-it.first;
+            if(mp.find(z)!=mp.end()){
+                if(z==it.first && it.second>=2){
+                    b = x;c = z;
+                    break;
+                }else if(z!=it.first){
+                    b = x;
+                    c = z;
                     break;
                 }
+            }
+            if(b!=-1){
+                break;
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b){
+                ans.push_back(i);
+            }
+            else if(a[i]==c){
+                ans.push_back(i);
             }
         }
         return ans;

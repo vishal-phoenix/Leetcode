80e03e1b77c3674f92bc46461fb815882c27ddd3
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2023-03-04 07:28:48 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (cpp)

--- my-folder/problems/binary_tree_maximum_path_sum/solution.cpp
+++ my-folder/problems/binary_tree_maximum_path_sum/solution.cpp
@@ -11,17 +11,20 @@
  */
 class Solution {
 public:
+    int DFS(TreeNode* root,int &maxsum){
+        if(root==NULL){
+            return 0;
+        }
+        int l = DFS(root->left,maxsum);
+        int r = DFS(root->right,maxsum);
+        maxsum = max(maxsum,max(l+root->val,max(r+root->val,max(l+r+root->val,root->val))));
+        return max(l+root->val,max(r+root->val,root->val));
+    }
     int maxPathSum(TreeNode* root) {
         int maxSum = INT_MIN;
         DFS(root, maxSum);
         return maxSum;
     }
     
-    int DFS(TreeNode* root, int& maxSum){
-        if(!root) return 0;
-        int left = max(0, DFS(root->left, maxSum));
-        int right = max(0, DFS(root->right, maxSum));
-        maxSum = max(maxSum, left + right + root->val);
-        return max(left, right) + root->val;
-    }
+    
 };

db2023e0748a8ed384e4644ac6911d967f201473
vishal-phoenix <100995024+vishal-phoenix@users.noreply.github.com>
2023-03-09 18:34:52 +0000

    Sync LeetCode submission - Binary Tree Right Side View (cpp)

--- my-folder/problems/binary_tree_right_side_view/solution.cpp
+++ my-folder/problems/binary_tree_right_side_view/solution.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     void recursion(TreeNode *root, int level, vector<int> &res)
